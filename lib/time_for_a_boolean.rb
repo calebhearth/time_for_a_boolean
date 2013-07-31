22c57e8126e393d7d3382c14f050d56edeb81dd2
Caleb Thompson <caleb@calebthompson.io>
2013-07-30 21:53:02 -0400

    That should be an alias

--- lib/time_for_a_boolean.rb
+++ lib/time_for_a_boolean.rb
@@ -3,6 +3,6 @@ require 'time_for_a_boolean/version'
 module TimeForABoolean
   def time_for_a_boolean(attribute)
     define_method(attribute) { true }
-    define_method("#{attribute}?") { send(attribute) }
+    alias_method "#{attribute}?", attribute
   end
 end

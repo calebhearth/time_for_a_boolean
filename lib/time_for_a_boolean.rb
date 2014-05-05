1a69714aeea29556d396c2174fd9144c04750741
John Mazzoli <jmazzoli@gmail.com>
2014-05-05 14:38:57 -0400

    Check for other representations of true and false

--- lib/time_for_a_boolean.rb
+++ lib/time_for_a_boolean.rb
@@ -1,4 +1,5 @@
 require 'rails'
+require 'active_record'
 require 'time_for_a_boolean/version'
 require 'time_for_a_boolean/railtie'
 
@@ -10,11 +11,15 @@ module TimeForABoolean
     end
     alias_method "#{attribute}?", attribute
     define_method("#{attribute}=") do |value|
-      if value
+      if self.class.value_for_boolean(value)
         send("#{attribute}_at=", -> { DateTime.current }.())
       else
         send("#{attribute}_at=", nil)
       end
     end
+  end
+
+  def value_for_boolean(value)
+    ActiveRecord::ConnectionAdapters::Column.value_to_boolean(value)
   end
 end
